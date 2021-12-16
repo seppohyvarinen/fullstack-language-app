7608f0aebd08eb7a4ab775f5ad5f33f6afdfa14c
Seppo Hyvärinen <seppo.hyvarinen@.tuni.fi>
2021-12-16 15:17:19 +0200

    Add: components for Teacher View, Student View and for selecting mode

---
+++ frontend/src/components/ModeSelect.js
@@ -0,0 +1,10 @@
+const ModeSelect = () => {
+  return (
+    <div className="Modes">
+      <div id="selectTeacher">Click to select Teacher Mode</div>
+      <div id="selectStudent">Click to select Student Mode</div>
+    </div>
+  );
+};
+
+export default ModeSelect;
