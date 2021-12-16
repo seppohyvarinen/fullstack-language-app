a5b9270e534c16f7f0e5638b13d6fc6b36561119
Seppo Hyvärinen <seppo.hyvarinen@.tuni.fi>
2021-12-16 15:40:16 +0200

    Add: User can now choose either mode, student or teacher

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import { useState } from "react";
 import ModeSelect from "./components/ModeSelect";
+import TeacherView from "./components/TeacherView";
+import StudentView from "./components/StudentView";
 
 const axios = require("axios").default;
 
 function App() {
   const [mode, Setmode] = useState(0);
 
-  return <div className="App">{mode === 0 && <ModeSelect />}</div>;
+  return (
+    <div className="App">
+      {mode === 0 && <ModeSelect Setmode={Setmode} />}
+      {mode === 1 && <TeacherView Setmode={Setmode} />}
+      {mode === 2 && <StudentView Setmode={Setmode} />}
+    </div>
+  );
 }
 
 export default App;
