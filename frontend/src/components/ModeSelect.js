22b536613ac3bae7255d7818f9a19984d61d2d71
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2021-12-16 15:40:16 +0200

    Add: User can now choose either mode, student or teacher

--- frontend/src/components/ModeSelect.js
+++ frontend/src/components/ModeSelect.js
@@ -1,8 +1,15 @@
-const ModeSelect = () => {
+const ModeSelect = ({ Setmode }) => {
+  const handleClick = (mode) => {
+    Setmode(mode);
+  };
   return (
     <div className="Modes">
-      <div id="selectTeacher">Click to select Teacher Mode</div>
-      <div id="selectStudent">Click to select Student Mode</div>
+      <div onClick={() => handleClick(1)} id="selectTeacher">
+        Click to select Teacher Mode
+      </div>
+      <div onClick={() => handleClick(2)} id="selectStudent">
+        Click to select Student Mode
+      </div>
     </div>
   );
 };
