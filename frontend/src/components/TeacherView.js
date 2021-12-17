d0c234fdb677e2a70c65ab7b34ec8e4e65c38fff
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2021-12-17 15:08:38 +0200

    Add: AddWords component for adding words to database

--- frontend/src/components/TeacherView.js
+++ frontend/src/components/TeacherView.js
@@ -1,27 +1,37 @@
 import { useState, useEffect } from "react";
+import AddWords from "./AddWords";
 
 const axios = require("axios").default;
 
 const TeacherView = () => {
   const [words, setWords] = useState([]);
 
-  useEffect(() => {
-    const fetchAll = async () => {
-      try {
-        var response = await axios.get("/translations");
+  useEffect(
+    () => {
+      const fetchAll = async () => {
+        try {
+          var response = await axios.get("/translations");
 
-        var mapped = response.data.map(
-          ({ finnish, english }) => finnish + " - " + english
-        );
-        setWords(mapped);
-      } catch (error) {
-        alert(error);
-      }
-    };
-    fetchAll();
-  }, []);
+          var mapped = response.data.map(
+            ({ finnish, english }) => finnish + " - " + english
+          );
+          setWords(mapped);
+        } catch (error) {
+          alert(error);
+        }
+      };
+      fetchAll();
+    },
+    [],
+    [words]
+  );
 
-  return <div>{words}</div>;
+  return (
+    <div>
+      <AddWords setWords={setWords} />
+      {words}
+    </div>
+  );
 };
 
 export default TeacherView;
