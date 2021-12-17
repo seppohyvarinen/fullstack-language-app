6b2f0a73043eb2f09034e539b991fe627a7b9872
Seppo Hyvärinen <seppo.hyvarinen@.tuni.fi>
2021-12-17 15:25:33 +0200

    Add: implement frontend of saving words to database

--- frontend/src/components/AddWords.js
+++ frontend/src/components/AddWords.js
@@ -1,5 +1,38 @@
-const AddWords = () => {
-  return <div>Component that adds words to the database</div>;
+import { useState } from "react";
+const axios = require("axios").default;
+
+const AddWords = ({ setWords }) => {
+  const [fin, setFin] = useState("");
+  const [eng, setEng] = useState("");
+  const [tag, setTag] = useState("");
+
+  const SaveWord = async () => {
+    try {
+      await axios.post("/translations", {
+        finnish: fin,
+        english: eng,
+        tag: tag,
+      });
+      setFin("");
+      setEng("");
+      setTag("");
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  const Modal = () => {
+    return (
+      <div>
+        <input type={"text"}></input>
+        <input type={"text"}></input>
+        <input type={"text"}></input>
+        <button onClick={() => SaveWord}></button>
+      </div>
+    );
+  };
+
+  return <div>Com{Modal}</div>;
 };
 
 export default AddWords;
