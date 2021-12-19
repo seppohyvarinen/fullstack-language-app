f022bfc20ea7a92e4af55f5d2ac30d3a5a678d77
Seppo Hyvärinen <seppo.hyvarinen@.tuni.fi>
2021-12-19 12:48:19 +0200

    Add: post method for posting tags to separate table

--- routes/routes.js
+++ routes/routes.js
@@ -28,4 +28,17 @@ translations.post("/", async (req, res) => {
   }
 });
 
+translations.post("/tag", async (req, res) => {
+  let tmp = req.body;
+
+  try {
+    let save = await connections.saveTag(tmp);
+    res.statusCode = 201;
+    res.send(save);
+  } catch (error) {
+    res.statusCode = 400;
+    res.send(`${res.statusCode} Bad Request: ${error}`);
+  }
+});
+
 module.exports = translations;
