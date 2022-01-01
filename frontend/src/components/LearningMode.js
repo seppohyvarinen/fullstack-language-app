836815d761296c87bc2909b3cd3b0de2ff10aac2
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2022-01-01 14:33:21 +0200

    Refactor: Create own component for selecting learning mode in student view

---
+++ frontend/src/components/LearningMode.js
@@ -0,0 +1,28 @@
+import FinEng from "../img/fin-eng.png";
+import EngFin from "../img/eng-fin.png";
+
+const LearningMode = () => {
+  return (
+    <div className="Modes">
+      <div className="FinEng">
+        Opettele sanoja Suomesta Englanniksi
+        <img
+          className="Flags"
+          src={FinEng}
+          alt="Suomen Lippu ja Britannian lippu"
+        />
+      </div>
+
+      <div className="EngFin">
+        Opettele sanoja Englannista Suomeksi
+        <img
+          className="Flags"
+          src={EngFin}
+          alt="Suomen Lippu ja Britannian lippu"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LearningMode;
