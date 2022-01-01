d3606ecde6c54d2cb08452661b1815d993a1f247
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2022-01-01 14:25:00 +0200

    Add: images of flags, css to student view

--- frontend/src/components/StudentView.js
+++ frontend/src/components/StudentView.js
@@ -1,8 +1,26 @@
+import FinEng from "../img/fin-eng.png";
+import EngFin from "../img/eng-fin.png";
+
 const StudentView = () => {
   return (
     <div className="Modes">
-      <div className="FinEng">Opettele sanoja Suomesta Englanniksi</div>
-      <div className="EngFin">Opettele sanoja Englannista Suomeksi</div>
+      <div className="FinEng">
+        Opettele sanoja Suomesta Englanniksi
+        <img
+          className="Flags"
+          src={FinEng}
+          alt="Suomen Lippu ja Britannian lippu"
+        />
+      </div>
+
+      <div className="EngFin">
+        Opettele sanoja Englannista Suomeksi
+        <img
+          className="Flags"
+          src={EngFin}
+          alt="Suomen Lippu ja Britannian lippu"
+        />
+      </div>
     </div>
   );
 };
