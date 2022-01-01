5ba150543e5867d121aa5e205f4488bf5c9e1292
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2022-01-01 14:54:45 +0200

    Add: WordGame component now fetches tags from backend

--- frontend/src/components/StudentView.js
+++ frontend/src/components/StudentView.js
@@ -1,9 +1,16 @@
 import { useState } from "react";
 import LearningMode from "./LearningMode";
+import WordGame from "./WordGame";
 
 const StudentView = () => {
   const [mode, setMode] = useState(0);
-  return <>{mode === 0 && <LearningMode setMode={setMode} />}</>;
+  return (
+    <>
+      {mode === 0 && <LearningMode setMode={setMode} />}
+      {mode === 1 && <WordGame gameMode={mode} />}
+      {mode === 2 && <WordGame gameMode={mode} />}
+    </>
+  );
 };
 
 export default StudentView;
--- frontend/src/components/WordGame.js
+++ frontend/src/components/WordGame.js
@@ -1,5 +1,32 @@
-const WordGame = () => {
-    return (  );
-}
+import { useState, useEffect } from "react";
+
+const axios = require("axios").default;
+
+const WordGame = ({ gameMode }) => {
+  const [tags, setTags] = useState([]);
+
+  const fetchTags = async () => {
+    try {
+      var response = await axios.get("/translations/tags");
+
+      var mapped = response.data.map(({ tag }) => (
+        <div className="Gametags">{tag}</div>
+      ));
+      setTags(mapped);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTags();
+  }, []);
+  return (
+    <>
+      You are playing with game mode {gameMode}
+      <div>{tags}</div>
+    </>
+  );
+};
 
 export default WordGame;
