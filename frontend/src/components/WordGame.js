a9c7549043e9e575f238be780eb65dca454372ea
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2022-01-02 14:40:18 +0200

    Add: function handlegamestart to handle starting the game screen with correct keyword(tag)

--- frontend/src/components/WordGame.js
+++ frontend/src/components/WordGame.js
@@ -7,12 +7,19 @@ const WordGame = ({ gameMode }) => {
   const [keyword, setKeyword] = useState("");
   const [gameOn, setGameOn] = useState(false);
 
+  const handleGameStart = (tag) => {
+    setKeyword(tag);
+    setGameOn(true);
+  };
+
   const fetchTags = async () => {
     try {
       var response = await axios.get("/translations/tags");
 
       var mapped = response.data.map(({ tag }) => (
-        <div className="Gametag">{tag}</div>
+        <div className="Gametag" onClick={() => handleGameStart(tag)}>
+          {tag}
+        </div>
       ));
       setTags(mapped);
     } catch (error) {
@@ -26,6 +33,8 @@ const WordGame = ({ gameMode }) => {
   return (
     <div className="GameMenu">
       You are playing with game mode {gameMode}
+      game screen is on {gameOn}
+      keyword is {keyword}
       <div className="TagList">{tags}</div>
     </div>
   );
