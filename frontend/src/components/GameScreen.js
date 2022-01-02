f7397c2a10edd30dc5b5d12c6f126ba188e6ae20
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2022-01-02 15:08:37 +0200

    Add: frontend for fetching words by tag with axios

--- frontend/src/components/GameScreen.js
+++ frontend/src/components/GameScreen.js
@@ -1,5 +1,31 @@
-const GameScreen = () => {
-  return <div>This is the game screen</div>;
+import { useState, useEffect } from "react";
+
+const axios = require("axios").default;
+
+const GameScreen = ({ keyword }) => {
+  const [words, setWords] = useState([]);
+  const fetchByTag = async (keyword) => {
+    try {
+      var response = await axios.get("/translations", {
+        params: {
+          tag: { keyword },
+        },
+      });
+      var mapped = response.data.map(({ fin, eng }) => (
+        <div>
+          {fin}
+          {eng}
+        </div>
+      ));
+      setWords(mapped);
+    } catch (error) {
+      alert(error);
+    }
+  };
+  useEffect(() => {
+    fetchByTag();
+  }, []);
+  return <div>{words}</div>;
 };
 
 export default GameScreen;
