a4021b539bc2fb4bd318aab3891860c4f7410329
Seppo Hyvärinen <seppo.hyvarinen@.tuni.fi>
2022-01-02 14:11:30 +0200

    Add: return button to student mode

--- frontend/src/components/StudentView.js
+++ frontend/src/components/StudentView.js
@@ -2,10 +2,11 @@ import { useState } from "react";
 import LearningMode from "./LearningMode";
 import WordGame from "./WordGame";
 
-const StudentView = () => {
+const StudentView = ({ Setmode }) => {
   const [mode, setMode] = useState(0);
   return (
     <>
+      <button onClick={() => Setmode(0)}>Takaisin alkuun</button>
       {mode === 0 && <LearningMode setMode={setMode} />}
       {mode === 1 && <WordGame gameMode={mode} />}
       {mode === 2 && <WordGame gameMode={mode} />}
