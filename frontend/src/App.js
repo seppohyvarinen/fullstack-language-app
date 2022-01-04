12f379b4c358d0436b87d17f8b40be007afa2c20
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2022-01-04 15:23:19 +0200

    Add: new state to control the amount of questions, axios.responses are now cut according to this amount

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,27 @@
-import { useState } from "react";
+import { React, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navi from "./components/Navi";
 import Home from "./components/Home";
 
 function App() {
   const [homeState, setHomeState] = useState(0);
+  const [amountState, setAmountState] = useState(5);
+
   return (
     <BrowserRouter>
       {" "}
       <div className="App">
-        <Navi />
-
+        <Navi />{" "}
         <div className="content">
           <Routes>
             <Route
               path="/"
               element={
-                <Home onClick={() => setHomeState(0)} state={homeState} />
+                <Home
+                  onClick={() => setHomeState(0)}
+                  state={homeState}
+                  amount={amountState}
+                />
               }
             />
           </Routes>
