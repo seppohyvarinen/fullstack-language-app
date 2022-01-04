b2c417e0d98a6fe5b01ad2d31d24c7cd780a2e4b
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2022-01-04 15:34:37 +0200

    Add: component Results

--- frontend/src/components/GameScreen.js
+++ frontend/src/components/GameScreen.js
@@ -67,26 +67,6 @@ const GameScreen = ({ keyword, gameMode, amount }) => {
       }
     });
 
-    function shuffle(array) {
-      let currentIndex = array.length,
-        randomIndex;
-
-      // While there remain elements to shuffle...
-      while (currentIndex != 0) {
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-
-        // And swap it with the current element.
-        [array[currentIndex], array[randomIndex]] = [
-          array[randomIndex],
-          array[currentIndex],
-        ];
-      }
-
-      return array;
-    }
-
     return (
       <div>
         <div className="Score">{score}</div>
@@ -139,6 +119,26 @@ const ArrayCutter = (arr, size) => {
   }
 
   return newArray;
+};
+
+const shuffle = (array) => {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
 };
 
 export default GameScreen;
