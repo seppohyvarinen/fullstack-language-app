53a9aab92b19bc9462ce32026906d4863b86eca1
Seppo Hyvärinen <seppo.hyvarinen@.tuni.fi>
2022-01-11 13:41:09 +0200

    Add: improve authentication to use JWT and cookies

--- connections/JWT/JWT.js
+++ connections/JWT/JWT.js
@@ -1,1 +1,13 @@
 const { sign, verify } = require("jsonwebtoken");
+
+const createTokens = (user) => {
+  const accessToken =
+    ({ username: user.username },
+    "jwtsecret",
+    {
+      expires_in: 400,
+    });
+
+  return accessToken;
+};
+module.exports = { createTokens };
