fbe385baf0725bca4319b5b96b44c42a637e328f
Seppo Hyvärinen <seppo.hyvarinen@.tuni.fi>
2022-01-11 16:46:30 +0200

    Add: css to menu screen, place back-button correctly

--- frontend/src/components/StudentView.js
+++ frontend/src/components/StudentView.js
@@ -6,7 +6,9 @@ const StudentView = ({ Setmode, amount }) => {
   const [mode, setMode] = useState(0);
   return (
     <>
-      <button onClick={() => Setmode(0)}>Takaisin alkuun</button>
+      <button className="Returnbtn" id="Menuscreen" onClick={() => Setmode(0)}>
+        Takaisin alkuun
+      </button>
       {mode === 0 && <LearningMode setMode={setMode} />}
       {mode === 1 && <WordGame gameMode={mode} amount={amount} />}
       {mode === 2 && <WordGame gameMode={mode} amount={amount} />}
