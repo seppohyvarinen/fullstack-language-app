a689e72a35fca4eb506320038cf5145663c9e431
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2022-01-11 18:50:10 +0200

    Documentation: documentation to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,13 @@ import Navi from "./components/Navi";
 import Home from "./components/Home";
 import Settings from "./components/Settings";
 
+/**
+ * This function is the main component of the translation application.
+ * Contains states that hold information about Home components states and amountState that
+ * holds information about amount the words used in the word game.
+ * @returns {BrowserRouter} that has wrapped all the Routes used in the app inside.
+ */
+
 function App() {
   const [homeState, setHomeState] = useState(0);
   const [amountState, setAmountState] = useState(5);
