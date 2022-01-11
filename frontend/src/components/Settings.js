7c91d3a52477da29e46ab41a1e5206f583136ea8
Seppo Hyvärinen <seppo.hyvarinen@.tuni.fi>
2022-01-11 20:33:04 +0200

    Add: About component, edited settings component

--- frontend/src/components/Settings.js
+++ frontend/src/components/Settings.js
@@ -1,35 +1,5 @@
 const Settings = ({ amount, setAmount }) => {
-  return (
-    <div className="Settings">
-      <div className="Amountset">
-        Kysymysten määrä:
-        <input
-          type="radio"
-          value="5"
-          name="amount"
-          checked={amount === 5 ? "checked" : null}
-          onClick={() => setAmount(5)}
-        />{" "}
-        5
-        <input
-          type="radio"
-          value="7"
-          name="amount"
-          checked={amount === 7 ? "checked" : null}
-          onClick={() => setAmount(7)}
-        />{" "}
-        7
-        <input
-          type="radio"
-          value="10"
-          name="amount"
-          checked={amount === 10 ? "checked" : null}
-          onClick={() => setAmount(10)}
-        />{" "}
-        10
-      </div>
-    </div>
-  );
+  return <div className="Settings">TULOSSA</div>;
 };
 
 export default Settings;
