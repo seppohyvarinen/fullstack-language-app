32439714b90fff5e27ad1490c77811df59ba9605
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2022-01-12 11:13:36 +0200

    Documentation: comment About.js, StudentView.js

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,3 +1,8 @@
+/**
+ * This component acts as a simple info page about the app.
+ * @returns {String} information about the app.
+ */
+
 const About = () => {
   return <div></div>;
 };
--- frontend/src/components/StudentView.js
+++ frontend/src/components/StudentView.js
@@ -2,6 +2,15 @@ import { useState } from "react";
 import LearningMode from "./LearningMode";
 import WordGame from "./WordGame";
 
+/**
+ * This component acts as the base component for the student view. It by default renders LearningMode component
+ * where user selects preferred mode. With mode selected the component renders the WordGame with specific mode.
+ * @param {Number} Setmode is setter for the Mode state in the Home component. It is used in a button and also passed forward
+ * for return purposes.
+ * @param {Number} amount is state inherited from Home component, defines the amount of words used in the game.
+ * @returns a button for returning and conditionally either mode select screen or the WordGame with specific mode.
+ */
+
 const StudentView = ({ Setmode, amount }) => {
   const [mode, setMode] = useState(0);
   return (
