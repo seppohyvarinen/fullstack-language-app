5d8a8bd0af2dfe17983f2c0bd18fbf511720c4a7
Seppo Hyvärinen <seppo.hyvarinen@.tuni.fi>
2022-01-12 13:41:42 +0200

    Documentation: commentEditModal.js

--- frontend/src/components/EditModal.js
+++ frontend/src/components/EditModal.js
@@ -2,6 +2,21 @@ import { useState } from "react";
 
 const axios = require("axios").default;
 
+/**
+ *
+ * @param {String} editFin is received as props and set to state fin so user is shown correct finnish translation of the selected word.
+ * @param {String} editEng is received as props and set to state eng so user is shown correct english translation of the selected word.
+ * @param setEdit is the setter received as props used to close the edit screen.
+ * @param fetchAll is the async function that fetches all words.
+ * Called when word is edited or deleted so user is shown updated word list.
+ * @param filterValue is filter value received as props so fetchByTag can be done with correct tag value
+ * @param fetchAll is the async function that fetches words by tag.
+ * Called when word is edited or deleted so user is shown updated word list.
+ * @param token is the JWT access token that is sent with patch and delete requests for authentication.
+ *
+ * @returns inputs and buttons as modal that use the functionality defined in this component.
+ */
+
 const EditModal = ({
   editFin,
   editEng,
@@ -23,6 +38,14 @@ const EditModal = ({
   const handleEng = (e) => {
     setEng(e.target.value);
   };
+
+  /**
+   * Async function that uses axios.patch to update fin_eng table in the database.
+   * Sends with the request the new english and finnish and also the old ones to the data is
+   * updated in correct rows in the database table. Also sends JWT access token for authentication.
+   * @param  f is the edited finnish translation that is sent to the database.
+   * @param  e is the edited english translation that is sent to the database.
+   */
 
   const saveEdit = async (f, e) => {
     try {
@@ -46,6 +69,13 @@ const EditModal = ({
       alert(error);
     }
   };
+
+  /**
+   * Async function that uses axios.delete to send delete request to backend. Sends finnish and
+   * english information and also JWT access token for authentication.
+   * @param  f finnish the word to be deleted.
+   * @param  e english for the word to be deleted.
+   */
 
   const deleteWord = async (f, e) => {
     try {
