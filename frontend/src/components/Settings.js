14016492c9c7df3dacab4d23e7a2d9908c53d1f4
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2022-01-12 14:27:52 +0200

    Add: possibility to change bg color from settings

--- frontend/src/components/Settings.js
+++ frontend/src/components/Settings.js
@@ -1,5 +1,12 @@
-const Settings = ({ amount, setAmount }) => {
-  return <div className="Settings">TULOSSA</div>;
+const Settings = ({ setColor }) => {
+  return (
+    <div className="Settings">
+      <h2>Vaihda taustaväriä: </h2>
+
+      <button onClick={() => setColor("default")}>Oletus</button>
+      <button onClick={() => setColor("morningLight")}>Morning Light</button>
+    </div>
+  );
 };
 
 export default Settings;
