5eefec1e109da926ac84eb9e49fc3cc454d2cb96
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2022-01-12 11:21:01 +0200

    Documentation: comment TeacherMode.js

--- frontend/src/components/TeacherView.js
+++ frontend/src/components/TeacherView.js
@@ -5,6 +5,17 @@ import EditModal from "./EditModal";
 
 const axios = require("axios").default;
 
+/**
+ * This componen is the base component of the teacher view. User can here modify the database, add translations, edit translations, add tags
+ * and also delete words. The component renders 2 sub components AddWords and EditModal that participate in providing
+ * this functionality.
+ * @param {Number} Setmode is the mode select setter inherited from Home component. Used here to return back.
+ * @param {String} token is the JWT token received upon succesful login. Used when making post/patch/delete requests to
+ * database for authentication.
+ * @param setToken is the setter for token state. Is set to empty string on logout.
+ * @returns Components EditModal, Addwords and all necessary buttons and arrays.
+ */
+
 const TeacherView = ({ Setmode, token, setToken }) => {
   const [words, setWords] = useState([]);
   const [tagsForFilter, setTagsForFilter] = useState([]);
