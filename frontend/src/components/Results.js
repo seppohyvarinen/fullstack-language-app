2816b72a5619cfc0802ff1eb1bd15e52d9979d7d
Seppo Hyvärinen <seppo.hyvarinen@.tuni.fi>
2022-01-12 15:00:55 +0200

    Documentation: comment Results.js

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -1,3 +1,10 @@
+/**
+ * This component is rendered when game is through.
+ * @param {number} score is the player score.
+ * @param {number} amount is the amount of questions in the game.
+ * @returns div that contains user score and a "reaction" according to user score.
+ */
+
 const Results = ({ score, amount }) => {
   var reaction = "";
   if (score === amount) {
